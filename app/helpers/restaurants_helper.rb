a5a5f7087e180fcc42e44b1bae1745fcc9bf5335
neillsilva <blackvector@gmail.com>
2014-10-31 16:11:26 -0400

    Awesome :zap:

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,2 +1,9 @@
 module RestaurantsHelper
+    def edit_or_add_restaurant
+        if params[:action] == 'edit'
+            return "Edit Restaurant"
+        else
+            return "Add Restaurant"
+        end
+    end
 end
